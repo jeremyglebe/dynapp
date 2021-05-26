6d2b8e61e99a643c056fbd4536dd7f4165aaa0fa
Jeremy Glebe <jeremyglebe@gmail.com>
2021-05-26 01:58:27 +0000

    get ticket by id

---
+++ get_ticket_by_id.php
@@ -0,0 +1,28 @@
+<?php
+require_once('services/common.php');
+require_once('services/db.php');
+require_once('services/ncode.php');
+
+try {
+    // Get data from post request
+    $post = common_post();
+    // Make sure that POST data includes a ticket_id
+    if (!isset($post["ticket_id"])) {
+        throw new Exception('ERROR: get_ticket_by_id.php; POST request must include property "ticket_id"!');
+    }
+    // Try to get an authenticated user
+    $user = common_user();
+    // Get the specified ticket, but don't provide it to the user yet
+    $tickets = db_get_ticket_by_id($post["ticket_id"]);
+    // User is admin or ticket belongs to user or ticket is unclaimed
+    if ($user["usertype_cd"] == 'A' || $tickets[0]["user_name"] == $user["user_name"] || $tickets[0]["user_name"] == '') {
+        // Provide a json of the tickets
+        echo ncode_json($tickets);
+    } else {
+        throw new Exception('ERROR: get_ticket_by_id.php; Requesting user lacks permission! (Must be admin or requesting their own ticket or requesting an unaccepted ticket)');
+    }
+}
+// Catch any errors and echo them back
+catch (Exception $e) {
+    common_echo_error($e);
+}
--- services/db.php
+++ services/db.php
@@ -145,6 +145,21 @@ function db_get_user_checklists($user_id)
     return $result;
 }
 
+function db_get_ticket_by_id($ticket_id)
+{
+    // Establish database connection
+    $conn = db_connect();
+    // Prepare the query, with the user field being unknown
+    $query = $conn -> prepare("SELECT * FROM tickets WHERE ticket_id=?;");
+    // Attach the username argument provided
+    $query -> bind_param("s", $ticket_id);
+    // Execute and store the result of the query
+    $query->execute();
+    $result = $query->get_result();
+    // Return the result processed into an array
+    return db_result_array($result);
+}
+
 function db_create_ticket($td)
 {
     // Establish database connection
