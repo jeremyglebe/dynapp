8218530aa0b1be5904fff645e4f51dbcc5547663
Jeremy Glebe <jeremyglebe@gmail.com>
2021-05-30 23:01:41 +0000

    get singular ticket in get ticket by id

--- get_ticket_by_id.php
+++ get_ticket_by_id.php
@@ -16,8 +16,8 @@ try {
     $tickets = db_get_ticket_by_id($post["ticket_id"]);
     // User is admin or ticket belongs to user or ticket is unclaimed
     if ($user["usertype_cd"] == 'A' || $tickets[0]["user_name"] == $user["user_name"] || $tickets[0]["user_name"] == '') {
-        // Provide a json of the tickets
-        echo ncode_json($tickets);
+        // Provide a json of the first (and hopefully only) ticket with that id
+        echo ncode_json($tickets[0]);
     } else {
         throw new Exception('ERROR: get_ticket_by_id.php; Requesting user lacks permission! (Must be admin or requesting their own ticket or requesting an unaccepted ticket)');
     }
