7a2548ef837055c1a18977dc65896596c0588b48
zachkingcade@gmail.com <zachkingcade@gmail.com>
2021-11-11 20:21:46 -0600

    quickly testing something

--- update_complete_ticket.php
+++ update_complete_ticket.php
@@ -12,7 +12,7 @@ try {
     $result = db_update_complete_ticket($post["ticket_id"], $user["user_name"], $post["comp_date"], $post["billing"], $post["summary"]);
     // Test for the result
     if ($result) {
-        common_echo_success("Ticket updated/completed in the database!");
+        common_echo_success("Ticket updated/completed in the database!", $post["ticket_id"], $user["user_name"], $post["comp_date"], $post["billing"], $post["summary"]);
     } else {
         throw new Exception("ERROR: update_complete_ticket.php; Ticket update failed! (CAUSE UNKNOWN)");
     }
